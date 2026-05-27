6048319bedbd253f03f987346cea322f0fe5dca6
ZhiDai <3240961776@qq.com>
2026-05-27 16:57:38 +0800

    Dev:空中跳跃功能制作

--- Assets/Script/PlayerJump.cs
+++ Assets/Script/PlayerJump.cs
@@ -14,8 +14,9 @@ public class PlayerJump : MonoBehaviour
     public Rigidbody2D Rb;//移动通过刚体组件完成 新建一个刚体组件类型的变量rb存储它的引用
     public bool IsGround;
     public AudioSource JumpSound;
-    private bool IsJumping;
+    private bool IsJumping;//用于跳跃手感优化,判定是否在进行跳跃,如果为true给Rb一个下坠值.
     private float JumpAddController;
+    private bool IsCanAirJump;
     
     void Start()
     {
@@ -31,9 +32,10 @@ public class PlayerJump : MonoBehaviour
         if (Input.GetButtonDown("Jump") && IsGround)
         {
             JumpSound.Play();
-            Rb.velocity = new Vector2(Rb.velocity.x, JumpSpeed);//跳跃
-            IsJumping = true;//跳跃手感优化
-            JumpAddController = 0;
+            Rb.velocity = new Vector2(Rb.velocity.x, JumpSpeed);//跳跃.
+            IsJumping = true;//跳跃手感优化.
+            JumpAddController = 0;//长按更高跳跃功能使用
+            IsCanAirJump = true;//跳跃后允许空中跳跃.
         }
         IsGround = Physics2D.Raycast(transform.position, Vector2.down, GroundCheak, GroundLayer);//地面检测——利用射线检测,让玩家在离开地面后无法再次跳跃.
         
@@ -42,15 +44,27 @@ public class PlayerJump : MonoBehaviour
             IsJumping = false;
         }
 
-        if (!IsJumping)
+        if (!IsJumping)//坠落效果
         {
             Rb.velocity -= new Vector2(0, -Physics2D.gravity.y/*获取引擎物理设置内的重力数值*/ * Time.deltaTime * FallAddPower);
-        }//用于跳跃手感优化,当玩家没有按下跳跃按键的时候,在Y轴添加一个向下的力
+        }//用于跳跃手感优化,当玩家没有按下跳跃按键的时候,在Y轴添加一个向下的力.
 
-        if (IsJumping && JumpAddController < JumpAddTime)
+        if (IsJumping )//长按更高跳跃功能
         {   
-            Rb.velocity += new Vector2(0, -Physics2D.gravity.y * Time.deltaTime * JumpAddPower);
-            JumpAddController += Time.deltaTime;
+            if(JumpAddController < JumpAddTime)
+                 Rb.velocity += new Vector2(0, -Physics2D.gravity.y * Time.deltaTime * JumpAddPower);//JumpAddPower控制增强跳跃高度
+            else
+                 IsJumping = false ;
+
+            JumpAddController += Time.deltaTime;//为JumpAddController增加数值
+        }
+        //空中跳跃
+        if(IsCanAirJump && !IsGround && Input.GetButtonDown("Jump")) //当IsCanAirJump为true,且玩家不在地面,允许玩家进行空中跳跃.
+        {
+            Rb.velocity = new Vector2(Rb.velocity.x, JumpSpeed);//二段跳,给Rb组件添加一个Y轴JumpSpeed速度.
+            IsJumping = true;
+            JumpAddController = 0;
+            IsCanAirJump = false;
         }
 
     }
